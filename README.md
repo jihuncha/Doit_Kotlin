f4a1db9a3ac84886cbf6aa4a32c68eb476afe12b
차지훈 <jihun.cha@tisquare.com>
2021-04-19 17:57:32 +0900

    readme update

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Doit_Kotlin
+
+
+# 2. 변수와 자료형 연산자.
+* val - 변경 불가능
+* var - 변경 가능
+
+~~~kotlin
+val username : String = "Kildong"
+~~~
+
+* 타입 추론이 가능 
+~~~kotlin
+val username = "Kildong"
+~~~
+
+* 코틀린 자료형은 참조형 자료형을 사용
---
+++ src/com/kotlin/kt_210419/section2/ValVar.kt
@@ -0,0 +1,12 @@
+package com.kotlin.kt_210419.section2
+
+fun main() {
+    val number = 10
+    var language = "Korean"
+    val secondNumber: Int = 20
+    language = "English"
+
+    println("number - $number")
+    println("language - $language")
+    println("secondNumber - $secondNumber")
+}
