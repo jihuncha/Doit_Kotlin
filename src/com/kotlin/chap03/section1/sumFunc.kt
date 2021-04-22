1da56a962ba8ba1512af24a4ba99955d8eac4d8a
차지훈 <jihun.cha@tisquare.com>
2021-04-22 17:40:30 +0900

    깔짝2

---
+++ src/com/kotlin/chap03/section1/sumFunc.kt
@@ -0,0 +1,14 @@
+package com.kotlin.chap03.section1
+
+//fun sum(a:Int, b:Int): Int {
+//    var sum = a + b
+//    return sum
+//}
+
+//fun sum(a:Int, b:Int): Int {
+//    return a + b
+//}
+
+//fun sum(a:Int, b:Int): Int = a + b
+
+fun sum(a:Int, b:Int) = a + b
